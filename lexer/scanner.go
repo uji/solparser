c3faef7f26b7b52beeb5f123fef722b66fdc0dd1
uji <ujiprog@gmail.com>
2022-05-13 23:53:26 +0900

    lexer: create scanner.go and move code for scanning

---
+++ lexer/scanner.go
@@ -0,0 +1,66 @@
+package lexer
+
+import "unicode/utf8"
+
+// isSpace reports whether the character is a Unicode white space character.
+// We avoid dependency on the unicode package, but check validity of the implementation
+// in the tests.
+func isSpace(r rune) bool {
+	if r <= '\u00FF' {
+		// Obvious ASCII ones: \t through \r plus space. Plus two Latin-1 oddballs.
+		switch r {
+		case ' ', '\t', '\n', '\v', '\f', '\r':
+			return true
+		case '\u0085', '\u00A0':
+			return true
+		}
+		return false
+	}
+	// High-valued ones.
+	if '\u2000' <= r && r <= '\u200a' {
+		return true
+	}
+	switch r {
+	case '\u1680', '\u2028', '\u2029', '\u202f', '\u205f', '\u3000':
+		return true
+	}
+	return false
+}
+
+func isMiscToken(r rune) bool {
+	return asMiscToken(r) != Unknown
+}
+
+func ScanTokens(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	// Skip leading spaces.
+	start := 0
+	for width := 0; start < len(data); start += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[start:])
+		if !isSpace(r) {
+			break
+		}
+	}
+	// Return misk token.
+	r, width := utf8.DecodeRune(data[start:])
+	if isMiscToken(r) {
+		return start + width, data[start : start+width], nil
+	}
+	// Scan until space, marking end of word.
+	for width, i := 0, start; i < len(data); i += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[i:])
+		if isMiscToken(r) {
+			return i, data[start:i], nil
+		}
+		if isSpace(r) {
+			return i + width, data[start:i], nil
+		}
+	}
+	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
+	if atEOF && len(data) > start {
+		return len(data), data[start:], nil
+	}
+	// Request more data.
+	return start, nil, nil
+}
---
+++ lexer/scanner_test.go
@@ -0,0 +1,55 @@
+package lexer_test
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/uji/solparser/lexer"
+)
+
+// Test that the token splitter.
+func TestScanTokens(t *testing.T) {
+	cases := []struct {
+		input string
+		want  []string
+	}{
+		{"", []string{}},
+		{" ", []string{" "}},
+		{"\n", []string{"\n"}},
+		{"a", []string{"a"}},
+		{" a ", []string{" ", "a", " "}},
+		{"abc  def ", []string{"abc", "  ", "def", " "}},
+		{"a\tb\nc\r\td\f", []string{"a", "\t", "b", "\n", "c", "\r\t", "d", "\f"}},
+		{"e\vf\u0085g\u00a0\n", []string{"e", "\v", "f", "\u0085", "g", "\u00a0", "\n"}},
+		{"^0.8.13", []string{"^", "0.8.13"}},
+		{"0.8.13;", []string{"0.8.13", ";"}},
+		{"pragma solidity ^0.8.13;", []string{"pragma", "solidity", "^", "0.8.13", ";"}},
+		{"contract HelloWorld {", []string{"contract", "HelloWorld", "{"}},
+	}
+
+	for n, c := range cases {
+		buf := strings.NewReader(c.input)
+		s := bufio.NewScanner(buf)
+		s.Split(lexer.ScanTokens)
+
+		got := make([]string, 0, len(c.want))
+		for i := 0; i < len(c.want); i++ {
+			if !s.Scan() {
+				break
+			}
+			got = append(got, s.Text())
+		}
+		if s.Scan() {
+			t.Errorf("#%d: scan ran too long, got %q", n, s.Text())
+		}
+		if diff := cmp.Diff(c.want, got); diff != "" {
+			t.Errorf("#%d: %s", n, diff)
+		}
+		err := s.Err()
+		if err != nil {
+			t.Errorf("#%d: %v", n, err)
+		}
+	}
+}
