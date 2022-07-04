69a8fa7d11f1896dd6721eb6bad47f1e3c423ef0
uji <ujiprog@gmail.com>
2022-07-05 02:35:45 +0900

    ast: implement literal rule

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,12 @@
 package ast
 
+import "github.com/uji/solparser/token"
+
+type Node interface {
+	Pos() token.Position
+	End() token.Position
+}
+
 type PragmaValue struct {
 	Version    string
 	Expression string
@@ -61,4 +68,67 @@ type SourceUnit struct {
 	PragmaDirective    *PragmaDirective
 	ContractDefinition *ContractDefinition
 	FunctionDefinition *FunctionDefinition
+}
+
+type Literal Node // BooleanLiteral | StringLiteral | NumberLiteral | HexStringLiteral | UnicordStringLiteral
+
+type BooleanLiteral struct {
+	Pos  token.Position
+	Bool bool
+}
+
+type StringLiteral struct {
+	Pos  token.Position
+	List []Node // EmptyStringLiteral | NonEmptyStringLiteral
+}
+
+type HexStringLiteral []*HexString
+
+type UnicordStringLiteral []*UnicordStrings
+
+type NumberLiteral struct {
+	Number     Node // DecimalNumber | HexNumber
+	NumberUnit *NumberUnit
+}
+
+type NumberUnit struct {
+	Pos   token.Position
+	Value string
+}
+
+type EmptyStringLiteral struct {
+	Pos          token.Position
+	SingleQuoted bool
+}
+
+type NonEmptyStringLiteral struct {
+	Pos  token.Position
+	List []Node // SingleQuotedPrintable | DoubleQuotedPrintable | EscapeSequence
+}
+
+type SingleQuotedPrintable struct {
+	Pos    token.Position
+	String string
+}
+
+type DoubleQuotedPrintable struct {
+	Pos    token.Position
+	String string
+}
+
+type EscapeSequence struct {
+	Pos    token.Position
+	String string
+}
+
+// unicode-string-literal (https://github.com/ethereum/solidity/blob/develop/docs/grammar/SolidityParser.g4#L407)
+type UnicordStrings struct {
+	Pos  token.Position
+	List []Node // string | EscapeSequence
+}
+
+type HexString struct {
+	Pos          token.Position
+	SingleQuoted bool
+	String       string
 }
